08b14c17afa4065ddac0899d9cd95b2ef35cdb75
Diego Tapia <tapiadiego123123@gmail.com>
2024-01-29 18:00:23 -0600

    updeate

--- dataBase.sql
+++ dataBase.sql
@@ -1,15 +1,21 @@
-CREATE TABLE empresa(
-  id_empresa SERIAL PRIMARY KEY NOT NULL,
-  nombre_empresa VARCHAR(100) NOT NULL
+
+CREATE TABLE materias(
+  id_materia SERIAL PRIMARY KEY NOT NULL,
+  nombre_materia VARCHAR(100) NOT NULL,
 );
 
-CREATE TABLE clientes(
-  id_cliente SERIAL PRIMARY KEY NOT NULL,
-  nombre VARCHAR(200) NOT NULL,
-  email VARCHAR(200) NOT NULL,
-  curp VARCHAR(13) NOT NULL,
-  rfc VARCHAR(16) NOT NULL,
-  telefono INT NOT NULL,
-  id_empresa INT NOT NULL,
- FOREIGN KEY (id_empresa) REFERENCES empresa(id_empresa)
+CREATE TABLE alumnos(
+  id_alumno SERIAL PRIMARY KEY NOT NULL,
+  nombre_alumno VARCHAR(255) NOT NULL,
+  apellido_alumno VARCHAR(255) NOT NULL,
+  cucorreo_alumno VARCHAR(255) NOT NULL,
+  telefono_alumno VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE alumnos_materias(
+  id_alumno_materia SERIAL PRIMARY KEY NOT NULL,
+  id_materia INT NOT NULL,
+  id_alumno INT NOT NULL,
+  FOREIGN KEY (id_materia)  REFERENCES materias(id_materia),
+  FOREIGN KEY (id_alumno)  REFERENCES alumnos(id_alumno)
 );
